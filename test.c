32910cfb82eba8da288da77147e71d2dac2b9c28
Aamir Javed <aamir.javed@oxeltech.de>
2024-06-28 16:56:35 +0300

    Updated code

--- test.c
+++ test.c
@@ -1,13 +1,19 @@
-include <stdint.h>
+#include <stdint.h>
+#include <stdio.h>
 
-uint32_t counter = 0u;
-for ( float32_t f = 0.0f; f < 1.0f; f += 0.001f )
-{
-++counter;
-}
+int main() {
 
-float32_t f;
-for ( uint32_t counter = 0u; counter < 1000u; ++counter )
-{
-f = ( float32_t ) counter * 0.001f;
+    uint32_t counter = 0u;
+    for ( float32_t f = 0.0f; f < 1.0f; f += 0.001f )
+    {
+        ++counter;
+    }
+
+    float32_t f;
+    for ( uint32_t counter = 0u; counter < 1000u; ++counter )
+    {
+        f = ( float32_t ) counter * 0.001f;
+    }
+
+    return 0;
 }
